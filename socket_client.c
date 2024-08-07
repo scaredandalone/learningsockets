094cb567989fd22e84f9f8325a459b819e837ad6
scaredandalone <144869567+scaredandalone@users.noreply.github.com>
2024-08-07 12:34:40 +1000

    Add files via upload

---
+++ socket_client.c
@@ -0,0 +1,34 @@
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    WSADATA wsastructure; // create wsa strucutre required for winsock
+    SOCKET client_socket; // create socket structure
+    int result;
+    struct sockaddr_in client_addr; // create struct required for connect()
+    char recvData[500];
+    char sendData[500] = "client successfully connected!";
+
+    result = WSAStartup(MAKEWORD(2, 2), &wsastructure); // initialise winsock1
+    if (result != 0)
+    {
+        printf("[!] Winsock initalisation failed!\n"); // print and exit
+        return 1;
+    }
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(9001);
+    client_addr.sin_addr.s_addr = inet_addr("192.168.11.128");
+
+    connect(client_socket, (SOCKADDR*) &client_addr, sizeof(client_addr)); // connect to target server
+    recv(client_socket, recvData, sizeof(recvData), 0); // receive data from server
+    printf("data from the server : %s\n", recvData); 
+    send(client_socket, sendData, sizeof(sendData), 0); // send data to server
+    closesocket(client_socket); // close socket and exit
+    WSACleanup(); // cleanup
+    return 0;
+}
---
+++ socket_server.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+int main()
+{
+	int server_socket; // create socket
+	int client_socket; // accept connection
+	struct sockaddr_in server_addr; // struct required to use bind()
+	char buffer[500] = "server is connected!";
+	char recv_buffer[500];
+	
+	server_socket = socket(AF_INET, SOCK_STREAM, 0); // TCP socket creation
+	
+	server_addr.sin_family = AF_INET; // declare the type
+	server_addr.sin_port = htons(9001); // listener port number
+	server_addr.sin_addr.s_addr = inet_addr("192.168.11.128"); // listener ip
+	
+	bind(server_socket, (struct server_addr*) &server_addr, sizeof(server_addr)); //bind server
+	listen(server_socket, 5);
+	
+	client_socket = accept(server_socket, NULL, NULL); // accept incoming connections
+	printf("client connected!\n");
+	send(client_socket, buffer, sizeof(buffer), 0); // send data to client
+	recv(client_socket, recv_buffer, sizeof(recv_buffer), 0); // recv data from client
+	printf("client has sent : %s", recv_buffer); // print data from client
+	close(server_socket); // close connection
+	return 0;
+} 
